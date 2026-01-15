511b0bf55a2485df6864f7ae8bf167df14d99ac4
hvo29 <hvo29@dxc.com>
2026-01-15 23:20:42 +0700

    15/01 Update Account module

--- src/main/java/com/bomrautft/common_util/util/constant/ApiURL.java
+++ src/main/java/com/bomrautft/common_util/util/constant/ApiURL.java
@@ -1,5 +1,5 @@
 package com.bomrautft.common_util.util.constant;
 
 public class ApiURL {
-    public static final String API_LEGEND = "legends";
+    public static final String API_LEGEND = "api/legends";
 }
